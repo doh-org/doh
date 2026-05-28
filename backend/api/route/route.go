732a1778756e5ae383a5e69f6a0eb289012b8c4f
oneyVerde <jhoney384@naver.com>
2026-05-28 20:44:21 +0900

    feat(back): route.Setup에 마커 라우터 등록

--- backend/api/route/route.go
+++ backend/api/route/route.go
@@ -11,4 +11,6 @@ import (
 func Setup(env *bootstrap.Env, keys map[string]*ecdsa.PublicKey, r *gin.Engine) {
 	v1 := r.Group("/api/v1")
 	NewAuthRouter(env, keys, v1.Group("/auth"))
+	tripsGroup := v1.Group("/trips")
+	NewMarkerRouter(env, keys, tripsGroup)
 }
