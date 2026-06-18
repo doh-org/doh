1907759225009d5ae77487204403a30f161ed4af
oneyVerde <jhoney384@naver.com>
2026-06-18 09:34:18 +0900

    fix(back): UserResponse user_id 필드 추가

--- backend/domain/user.go
+++ backend/domain/user.go
@@ -30,6 +30,7 @@ type LoginRequest struct {
 }
 
 type UserResponse struct {
+	UserID    string    `json:"user_id"`
 	Email     string    `json:"email"`
 	Nickname  string    `json:"nickname"`
 	CreatedAt time.Time `json:"created_at"`
